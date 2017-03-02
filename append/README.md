210424561f7b1a2c3ecdeb674662dd65940dfbd5
K <x00.x7f@gmail.com>
2017-03-02 16:33:50 +0900

    Added append benchmark

---
+++ append/README.md
@@ -0,0 +1,9 @@
+append benchmark
+=============================
+
+```
+BenchmarkNormal-4      	10000000	       230 ns/op	     122 B/op	       1 allocs/op
+BenchmarkAllocated-4   	20000000	       157 ns/op	      88 B/op	       1 allocs/op
+PASS
+ok  	github.com/Code-Hex/go-benchmarklist/append	6.605s
+```
---
+++ append/append_test.go
@@ -0,0 +1,48 @@
+package main
+
+import "testing"
+
+type Person struct {
+	Name   string
+	Age    int
+	Belong string
+	Rank   rune
+	Test
+}
+
+type Test struct {
+	Title   string
+	Score   uint
+	Average float64
+}
+
+func New() *Person {
+	return &Person{
+		Name:   "CodeHex",
+		Age:    10,
+		Belong: "College",
+		Rank:   'A',
+		Test: Test{
+			Title:   "Japanese",
+			Score:   80,
+			Average: 64.5,
+		},
+	}
+
+}
+
+func BenchmarkNormal(b *testing.B) {
+	b.ResetTimer()
+	var people []*Person
+	for i := 0; i < b.N; i++ {
+		people = append(people, New())
+	}
+}
+
+func BenchmarkAllocated(b *testing.B) {
+	b.ResetTimer()
+	people := make([]*Person, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		people = append(people, New())
+	}
+}
